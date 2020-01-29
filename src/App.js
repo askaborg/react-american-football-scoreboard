79335c04019f646d1cb18a2f009ca35d8a620922
askaborg <hienle@askaborg.com>
2020-01-29 02:37:20 -0800

    I need props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.css"
 import BottomRow from "./BottomRow"
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const initialState = 32
   const [lionsScore, setLions] = useState(initialState)
   const [tigersScore, setTigers] = useState(initialState)
@@ -12,35 +11,43 @@ function App() {
   return (
     <div className="container">
       <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{ lionsScore }</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{ tigersScore }</div>
-          </div>
-        </div>
+        <Top />
         <BottomRow />
       </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={ () => setLions(lionsScore + 7) }>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={ () => setLions(lionsScore + 3) }>Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={ () => setTigers(tigersScore + 7) }>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={ () => setTigers(tigersScore + 3) }>Away Field Goal</button>
-        </div>
-      </section>
+      <Buttons />
     </div>
-  );
+  )
+}
+
+function Top() {
+  return (
+    <div className="topRow">
+      <div className="home">
+        <h2 className="home__name">Lions</h2>
+        <div className="home__score">{ 32 }</div>
+      </div>
+        <div className="timer">00:03</div>
+        <div className="away">
+            <h2 className="away__name">Tigers</h2>
+            <div className="away__score">{ 32 }</div>
+        </div>
+    </div>
+  )
+}
+
+function Buttons() {
+  return (
+    <section className="buttons">
+      <div className="homeButtons">
+        <button className="homeButtons__touchdown">Home Touchdown</button>
+        <button className="homeButtons__fieldGoal">Home Field Goal</button>
+      </div>
+      <div className="awayButtons">
+        <button className="awayButtons__touchdown">Away Touchdown</button>
+        <button className="awayButtons__fieldGoal">Away Field Goal</button>
+      </div>
+      </section>
+  )
 }
 
 export default App
